69b90cf0a97e06837d0ed1b96877bacb78c32631
Bambang Ramadhan <bambangramadhan96@gmail.com>
2021-01-13 16:57:28 +0700

    Add new HomeStack

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import IconButton from 'react-native-vector-icons/MaterialCommunityIcons'
+
 import HomeScreen from '../screens/Home/HomeScreen';
+import AddRoomScreen from '../screens/Room/AddRoomScreen';
 
 const Stack = createStackNavigator();
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+const ChatApp = () => {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee'
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22
+        }
+      }}
+    >
+      <ChatAppStack.Screen
+        name={"Home"}
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <IconButton
+              icon='message-plus'
+              size={28}
+              color='#ffffff'
+              onPress={() => navigation.navigate('AddRoom')}
+            />
+          )
+        })}
+      />
+    </ChatAppStack.Navigator>
+  )
+}
 
 const HomeStack = () => {
   return (
-    <Stack.Navigator initialRouteName={"Home"} >
-      <Stack.Screen name={"Home"} component={HomeScreen} />
-    </Stack.Navigator>
+    <ModalStack.Navigator mode={'modal'} headerMode={'none'}>
+      <ModalStack.Screen name={'ChatApp'} component={ChatApp} />
+      <ModalStack.Screen name={'AddRoom'} component={AddRoomScreen} />
+    </ModalStack.Navigator>
   )
-}
+};
 
 export default HomeStack;
